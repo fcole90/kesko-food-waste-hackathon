affed5a49484d0e5c677d3fb591b962e87a5e42d
Fabio Colella <fcole90@gmail.com>
2019-11-15 21:50:21 +0200

    Initialise django setup

---
+++ kesko_food_waste/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kesko_food_waste project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kesko_food_waste.settings')
+
+application = get_wsgi_application()
---
+++ kesko_webapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KeskoWebappConfig(AppConfig):
+    name = 'kesko_webapp'
